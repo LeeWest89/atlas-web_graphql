fd78af0bd1b04c51d41c15e4c0ef31ef4d71406e
LeeWest89 <6683@holbertonstudents.com>
2024-07-02 18:22:48 -0500

    I think I almost have it for setting up the mongodb but I'm running into docker errors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema');
+const mongoose = require('mongoose');
 
 const app = express();
+const mongo = 'mongodb+srv://leewest:haCVed57qXMLJGoq@cluster0.pzumxsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.use('/graphql',graphqlHTTP({
   schema: schema,
