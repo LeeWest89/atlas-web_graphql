7a380ac8caeda5090280d4e71dc5e4a176c02ab4
LeeWest89 <6683@holbertonstudents.com>
2024-07-02 16:42:03 -0500

    I think I have everything working correctly through task 2, I was having an issue in graphiql2, I was using single instead of double quotes

--- server/schema.js
+++ server/schema.js
@@ -1,1 +1,46 @@
-#include
+// Schema for graphql project
+const gql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = gql;
+const lodash = require('lodash');
+
+const taskObjects = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+  }
+];
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return (lodash.find(taskObjects, { id: args.id }));
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
