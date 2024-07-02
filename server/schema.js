1718a24b12a44bd2a5ba00b985cb8d4bb2fe07b4
LeeWest89 <6683@holbertonstudents.com>
2024-07-02 17:37:49 -0500

    5 was pretty easy add tasks and project returns to RootQuery

--- server/schema.js
+++ server/schema.js
@@ -82,6 +82,18 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         return (lodash.find(projects, { id: args.id }));
       }
+    },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return (tasks);
+      }
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return (projects);
+      }
     }
   }
 });
